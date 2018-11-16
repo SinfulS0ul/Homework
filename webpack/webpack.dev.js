d8b0e30b1c47ace934a7ef5ef38e46bdad3b6d1f
SinfulS0ul <pasichnikmaks@gmail.com>
2018-11-16 06:25:40 +0200

    create project

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/js/main.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [{
+        test: /\.js?$/,
+        exclude: [/(node_modules)/, /\.spec\.js$/],
+        include: [
+          path.resolve(__dirname, '../js')
+        ],
+        use: []
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader',
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin(
+      '[name].css'
+    ),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html')
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, '../'),
+    compress: true,
+    historyApiFallback: true
+  }
+};
